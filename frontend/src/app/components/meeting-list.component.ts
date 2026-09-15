561b0d359d431029cd7afe4a6097ff21a621a006
Ilija <ilijaerak11@gmail.com>
2026-09-15 20:27:14 +0200

    Redesign meeting list with status badges and card layout

--- frontend/src/app/components/meeting-list.component.ts
+++ frontend/src/app/components/meeting-list.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { MeetingService } from '../services/meeting.service';
 import { Meeting } from '../models/meeting.model';
-import { NgIf, NgForOf, DatePipe, AsyncPipe } from '@angular/common';
+import { NgIf, NgForOf, NgClass, DatePipe, AsyncPipe, DecimalPipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
   selector: 'app-meeting-list',
   templateUrl: './meeting-list.component.html',
   styleUrls: ['./meeting-list.component.scss'],
-  imports: [NgIf, NgForOf, AsyncPipe, DatePipe, RouterModule, MatCardModule, MatButtonModule]
+  imports: [NgIf, NgForOf, NgClass, AsyncPipe, DatePipe, DecimalPipe, RouterModule, MatCardModule, MatButtonModule, MatIconModule]
 })
 export class MeetingListComponent {
   meetings$?: Observable<Meeting[]>;
